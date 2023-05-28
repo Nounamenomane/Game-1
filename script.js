6a4188576de3e8f515df567e032218586d173470
nounamenomane <nounamenomane@gmail.com>
2023-05-28 19:17:30 +0600

    page refresh

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ let gap = 90;
 // при нажатии на какую либо кнопку
 
 document.addEventListener("keydown", moveUp);
+document.addEventListener("click", moveUp);
 
 function moveUp() {
   yPos -= 25;
